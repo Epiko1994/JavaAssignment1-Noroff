158f3a8d3739d6039a466baadf33cc0142aa434d
Joshua Brian Johansen <joshuabrian.johansen@dk.experis.com>
2022-02-16 15:46:29 +0100

    Added getters and setters to Armour

--- src/Armour.java
+++ src/Armour.java
@@ -19,6 +19,46 @@ public class Armour extends Item {
     private String armourClass;
     private String armourSlot;
 
+    public int getBonusStrength() {
+        return bonusStrength;
+    }
+
+    public void setBonusStrength(int bonusStrength) {
+        this.bonusStrength = bonusStrength;
+    }
+
+    public int getBonusDexterity() {
+        return bonusDexterity;
+    }
+
+    public void setBonusDexterity(int bonusDexterity) {
+        this.bonusDexterity = bonusDexterity;
+    }
+
+    public int getBonusIntelligence() {
+        return bonusIntelligence;
+    }
+
+    public void setBonusIntelligence(int bonusIntelligence) {
+        this.bonusIntelligence = bonusIntelligence;
+    }
+
+    public String getArmourClass() {
+        return armourClass;
+    }
+
+    public void setArmourClass(String armourClass) {
+        this.armourClass = armourClass;
+    }
+
+    public String getArmourSlot() {
+        return armourSlot;
+    }
+
+    public void setArmourSlot(String armourSlot) {
+        this.armourSlot = armourSlot;
+    }
+
     public void generateStartingArmour() {
 
     }
